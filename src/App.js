95753c2239167214ab0c32ee9aea634257def9db
Pandouby <57749377+Pandouby@users.noreply.github.com>
2021-10-01 11:20:42 +0200

    first commit react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Timer from './Timer';
+
+export default function App() {
+    return (
+      <div>
+        <Timer />
+      </div>
+    );
+}
---
+++ src/Timer.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './App.css';
+import React from 'react';
+import { useStopwatch } from 'react-timer-hook';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, InputGroup, FormControl} from 'react-bootstrap';
+
+//Build with: INLINE_RUNTIME_CHUNK=false npm run build
+export default function Timer() {
+    const {
+        seconds,
+        minutes,
+        hours,
+        days,
+        isRunning,
+        start,
+        pause,
+        reset,
+    } = useStopwatch({ autoStart: false });
+
+    const nf = (number) => number >= 10 ? number : "0"+number
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <InputGroup size="sm" className="mb-3">
+                    <InputGroup.Text id="inputGroup-sizing-sm">Start Link</InputGroup.Text>
+                    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                </InputGroup>
+
+                <InputGroup size="sm" className="mb-3">
+                    <InputGroup.Text id="inputGroup-sizing-sm">End Link</InputGroup.Text>
+                    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                </InputGroup>
+
+                <span><h2>{nf(minutes)}:{nf(seconds)}</h2></span>
+                <span>
+                    <Button onClick={start} variant="primary">Start</Button>
+                    <Button onClick={pause} variant="primary">Stop</Button>
+                </span>
+                
+            </header>
+        </div>
+    );
+}
